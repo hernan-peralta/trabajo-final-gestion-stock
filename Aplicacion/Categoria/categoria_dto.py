686a56e272a867073f39bc81e996a924d0c83400
Hernán Peralta <alegodemencia@gmail.com>
2022-03-04 00:02:41 -0300

    Corrige atributo en categoria_dto

--- Aplicacion/Categoria/categoria_dto.py
+++ Aplicacion/Categoria/categoria_dto.py
@@ -2,4 +2,4 @@ class CategoriaDTO:
     def __init__(self, categoria_id, nombre, observaciones) -> None:
         self.id = categoria_id
         self.nombre = nombre
-        self.marca = observaciones
+        self.observaciones = observaciones
