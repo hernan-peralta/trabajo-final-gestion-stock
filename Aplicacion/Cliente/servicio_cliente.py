20f5a9676675f1d23fcaff16857e1ec83099c0c8
Hernán Peralta <alegodemencia@gmail.com>
2022-03-04 00:05:25 -0300

    Agrega funcionalidad en servicio_cliente

--- Aplicacion/Cliente/servicio_cliente.py
+++ Aplicacion/Cliente/servicio_cliente.py
@@ -12,11 +12,33 @@ class ServicioCliente:
         lista_clientes = []
         lista_query = self.repositorioCliente.obtener_todos()
         for (q) in lista_query:
-            lista_clientes.append(ClienteDTO(q[0], q[1], q[2], q[3], q[4], q[5], q[6], q[7], q[9]))
+            lista_clientes.append(ClienteDTO(q[0], q[1], q[2], q[3], q[4], q[5], q[6], q[7]))
         return lista_clientes
 
-    def obtener_id(self, cliente_id):
-        return self.repositorioCliente.obtener(cliente_id)
+    def buscar_por_nombre(self, nombre):
+        lista_clientes = []
+        lista_query = self.repositorioCliente.buscar_por_nombre(nombre)
+        for (q) in lista_query:
+            lista_clientes.append(ClienteDTO(q[0], q[1], q[2], q[3], q[4], q[5], q[6], q[7]))
+        return lista_clientes
+
+    def buscar_por_apellido(self, apellido):
+        lista_clientes = []
+        lista_query = self.repositorioCliente.buscar_por_apellido(apellido)
+        for (q) in lista_query:
+            lista_clientes.append(ClienteDTO(q[0], q[1], q[2], q[3], q[4], q[5], q[6], q[7]))
+        return lista_clientes
 
     def buscar_por_localidad(self, localidad):
-        return self.repositorioCliente.buscar_por_localidad(localidad)
+        lista_clientes = []
+        lista_query = self.repositorioCliente.buscar_por_localidad(localidad)
+        for (q) in lista_query:
+            lista_clientes.append(ClienteDTO(q[0], q[1], q[2], q[3], q[4], q[5], q[6], q[7]))
+        return lista_clientes
+
+    def buscar_por_provincia(self, provincia):
+        lista_clientes = []
+        lista_query = self.repositorioCliente.buscar_por_provincia(provincia)
+        for (q) in lista_query:
+            lista_clientes.append(ClienteDTO(q[0], q[1], q[2], q[3], q[4], q[5], q[6], q[7]))
+        return lista_clientes
