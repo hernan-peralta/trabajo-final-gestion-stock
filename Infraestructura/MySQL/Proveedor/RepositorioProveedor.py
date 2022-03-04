243350912f90f3cde884770fe22efe637409dfa5
Hernán Peralta <alegodemencia@gmail.com>
2022-03-04 00:04:16 -0300

    Agrega funcionalidad en RepositorioProveedor

--- Infraestructura/MySQL/Proveedor/RepositorioProveedor.py
+++ Infraestructura/MySQL/Proveedor/RepositorioProveedor.py
@@ -8,28 +8,34 @@ class RepositorioProveedor:
                              "(nombre, direccion_comercial, id_localidad, telefono, email, dni, cbu, banco_cbu, cuit, categoria_iva, observaciones) "
                              "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
         datos_proveedor = (
-        proveedor.nombre, proveedor.direccion_comercial, proveedor.id_localidad, proveedor.telefono, proveedor.email,
-        proveedor.dni, proveedor.cbu, proveedor.banco_cbu, proveedor.cuit, proveedor.categoria_iva,
-        proveedor.observaciones)
+            proveedor.nombre, proveedor.direccion_comercial, proveedor.id_localidad, proveedor.telefono,
+            proveedor.email,
+            proveedor.dni, proveedor.cbu, proveedor.banco_cbu, proveedor.cuit, proveedor.categoria_iva,
+            proveedor.observaciones)
         self.cursor.execute(agregar_proveedor, datos_proveedor)
         self.cnx.commit()
 
     def obtener_todos(self):
-        self.cursor.execute("SELECT * FROM proveedores")
+        self.cursor.execute(
+            "SELECT proveedores.id, proveedores.nombre, proveedores.direccion_comercial, l.nombre, proveedores.telefono, proveedores.email, proveedores.dni, proveedores.cbu, proveedores.banco_cbu, proveedores.cuit, c_i.categoria_iva, proveedores.observaciones FROM proveedores JOIN localidades AS l ON proveedores.id_localidad = l.id JOIN categoria_iva AS c_i on proveedores.id_categoria_iva = c_i.id")
         return self.cursor.fetchall()
 
     def buscar_por_nombre(self, nombre_proveedor):
         self.cursor.execute(
-            "SELECT proveedores.id, proveedores.nombre, proveedores.direccion_comercial, l.nombre, proveedores.telefono, proveedores.email, proveedores.dni, proveedores.cbu, proveedores.banco_cbu, proveedores.cuit, c_i.nombre, proveedores.observaciones FROM proveedores JOIN localidades AS l ON proveedores.id_localidad = l.id JOIN categoria_iva AS c_i on proveedores.id_categoria_iva = c_i.id WHERE proveedores.nombre LIKE %s",
+            "SELECT proveedores.id, proveedores.nombre, proveedores.direccion_comercial, l.nombre, proveedores.telefono, proveedores.email, proveedores.dni, proveedores.cbu, proveedores.banco_cbu, proveedores.cuit, c_i.categoria_iva, proveedores.observaciones FROM proveedores JOIN localidades AS l ON proveedores.id_localidad = l.id JOIN categoria_iva AS c_i on proveedores.id_categoria_iva = c_i.id WHERE proveedores.nombre LIKE %s",
             (nombre_proveedor.title(),))
         return self.cursor.fetchall()
 
-    def buscar_por_localidad(self, localidad_proveedor):
-        self.cursor.execute("SELECT * FROM proveedores WHERE localidad LIKE %s", (localidad_proveedor,))
+    def buscar_por_localidad(self, localidad):
+        self.cursor.execute(
+            "SELECT proveedores.id, proveedores.nombre, proveedores.direccion_comercial, l.nombre, proveedores.telefono, proveedores.email, proveedores.dni, proveedores.cbu, proveedores.banco_cbu, proveedores.cuit, c_i.categoria_iva, proveedores.observaciones FROM proveedores JOIN localidades AS l ON proveedores.id_localidad = l.id JOIN categoria_iva AS c_i on proveedores.id_categoria_iva = c_i.id WHERE l.nombre LIKE %s",
+            (localidad.title(),))
         return self.cursor.fetchall()
 
-    def buscar_por_provincia(self, provincia_proveedor):
-        self.cursor.execute("SELECT * FROM proveedores WHERE provincia LIKE %s", (provincia_proveedor,))
+    def buscar_por_provincia(self, provincia):
+        self.cursor.execute(
+            "SELECT proveedores.id, proveedores.nombre, proveedores.direccion_comercial, l.nombre, proveedores.telefono, proveedores.email, proveedores.dni, proveedores.cbu, proveedores.banco_cbu, proveedores.cuit, c_i.categoria_iva, proveedores.observaciones FROM proveedores JOIN localidades AS l ON proveedores.id_localidad = l.id JOIN provincias AS p ON l.id_provincia = p.id JOIN categoria_iva AS c_i on proveedores.id_categoria_iva = c_i.id WHERE p.nombre LIKE %s",
+            (provincia.title(),))
         return self.cursor.fetchall()
 
     def eliminar(self, id_proveedor):
