ea8ceb0e4878dd0c7fd0303e66b96964bcb6c5fb
Hernán Peralta <alegodemencia@gmail.com>
2022-03-08 00:04:10 -0300

    Actualiza lista de parametros

--- Gui/aplicacion.py
+++ Gui/aplicacion.py
@@ -4,16 +4,20 @@ from Gui.ui import *
 
 
 class Gui(QMainWindow):
-    def __init__(self, servicio_producto, servicio_cliente, servicio_proveedor, servicio_categoria):
+    def __init__(self, servicio_producto, servicio_cliente, servicio_proveedor, servicio_categoria, servicio_localidad,
+                 servicio_banco):
         super().__init__()
 
         self.ventana = Ui_MainWindow()
-        self.ventana.setupUi(self, servicio_producto, servicio_cliente, servicio_proveedor, servicio_categoria)
+        self.ventana.setupUi(self, servicio_producto, servicio_cliente, servicio_proveedor, servicio_categoria,
+                             servicio_localidad, servicio_banco)
         self.show()
 
 
-def crear_aplicacion(servicio_producto, servicio_cliente, servicio_proveedor, servicio_categoria):
+def crear_aplicacion(servicio_producto, servicio_cliente, servicio_proveedor, servicio_categoria, servicio_localidad,
+                     servicio_banco):
     application = QApplication(sys.argv)
-    dialog = Gui(servicio_producto, servicio_cliente, servicio_proveedor, servicio_categoria)
+    dialog = Gui(servicio_producto, servicio_cliente, servicio_proveedor, servicio_categoria, servicio_localidad,
+                 servicio_banco)
     dialog.show()
     sys.exit(application.exec_())
