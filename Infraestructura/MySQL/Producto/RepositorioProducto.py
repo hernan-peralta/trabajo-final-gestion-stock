ebfebfcbace63e9528ed218ffac52ad7ca375094
Hernán Peralta <alegodemencia@gmail.com>
2022-03-10 00:20:18 -0300

    Elimina comentario en RepositorioProducto

--- Infraestructura/MySQL/Producto/RepositorioProducto.py
+++ Infraestructura/MySQL/Producto/RepositorioProducto.py
@@ -4,7 +4,6 @@ class RepositorioProducto:
         self.cursor = cnx.cursor()
 
     def guardar(self, producto):
-        # TODO asociar la categoria
         agregar_producto = ("INSERT INTO productos "
                             "(nombre, marca, precio_unitario, unidades_stock, descripcion, observaciones) "
                             "VALUES (%s, %s, %s, %s, %s, %s)")
