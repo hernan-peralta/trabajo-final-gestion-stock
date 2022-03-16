c582918fe1b2b77cb365d716dc1c8ff42dae98d2
Hernán Peralta <alegodemencia@gmail.com>
2022-03-16 09:08:24 -0300

    Actualiza servicio detalle venta

--- Aplicacion/DetalleVenta/servicio_detalle_venta.py
+++ Aplicacion/DetalleVenta/servicio_detalle_venta.py
@@ -16,15 +16,15 @@ class ServicioDetalleVenta:
         cliente = self.repositorio_cliente.buscar_por_id(detalle_venta_request["cliente"])
         forma_pago = self.repositorio_forma_pago.buscar_por_nombre(detalle_venta_request["forma_pago"])
         venta_id = self.repositorio_venta.crear(cliente[0], forma_pago[0])
-        for item in detalle_venta_request["lista"]:
-            query_producto = self.repositorio_producto.buscar_por_nombre(item["producto"])
-            producto = Producto(query_producto[0][1], query_producto[0][2], query_producto[0][3], query_producto[0][4],
-                                query_producto[0][5], query_producto[0][6])
-            detalle_venta = DetalleVenta(query_producto[0][0], item["precio"], item["cantidad"], venta_id)
+        for detalle_venta in detalle_venta_request["lista"]:
+            query_producto = (self.repositorio_producto.buscar_por_nombre(detalle_venta["producto"]))[0]
+            producto = Producto(query_producto[1], query_producto[2], query_producto[3], query_producto[4],
+                                query_producto[5], query_producto[6])
+            detalle_venta = DetalleVenta(query_producto[0], detalle_venta["precio"], detalle_venta["cantidad"], venta_id)
             if producto.unidades_stock > 0:
                 self.repositorio_detalle_venta.guardar(detalle_venta)
                 producto.unidades_stock -= int(detalle_venta.cantidad)
-                self.repositorio_producto.actualizar(producto, query_producto[0][0])
+                self.repositorio_producto.actualizar(producto, query_producto[0])
 
     def obtener_por_venta_id(self, venta_id):
         lista_detalle_ventas = []
